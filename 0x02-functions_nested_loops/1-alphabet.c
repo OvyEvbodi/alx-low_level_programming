2303bcd3e8fe3433ff5d1270cfa9afc3cf4c2200
OvyEvbodi <evbodiovo@gmail.com>
2022-09-13 10:35:12 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: programs returns 0 on success
+*/
+
+int main (void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,15 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+
+/** print_alphabet - prints a - z in lower case 
+ *
+ * Return: void
+ */
+char print_alphabet(void);
+char a[27] = "abcdefghijklmnopqrstuvwxyz\n";
+
+char print_alphabet(void)
+{
+        return(write(1, &a, 27));
+}
