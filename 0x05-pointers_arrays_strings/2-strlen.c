8e2c3633be549ecb6c5b679d5edcb8c5f8410823
OvyEvbodi <evbodiovo@gmail.com>
2022-09-19 10:47:10 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to be evaluated
+ *
+ * Return: the number of characters in s
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
