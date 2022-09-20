db24da6449a1d39d9ef2230f6919e152606963b0
OvyEvbodi <evbodiovo@gmail.com>
2022-09-19 18:20:51 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,15 +12,23 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int j;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == '\0')
-		{
-			break;
-		}
-		_putchar(str[i]);
-		i = i + 2;
+		/**
+		* if (str[i] == '\0')
+		*{
+		*	break;
+		*}
+		*_putchar(str[i]);
+		*i = i + 2;
+		*/
+	}
+
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
