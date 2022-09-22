8bc1cc8c319a3e42e76955a9439b20e2be0e93aa
OvyEvbodi <evbodiovo@gmail.com>
2022-09-22 14:35:24 -0700

    encode a string into leet/ 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: the string to encode
+ *
+ * Return: a pointer to the encoded string
+ */
+
+char *leet(char *str)
+{
+	int i = 0;
+
+	for (; str[i]; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+			str[i] = '4';
+		else if (str[i] == 'e' || str[i] == 'E')
+			str[i] = '3';
+		else if (str[i] == 'o' || str[i] == 'O')
+			str[i] = '0';
+		else if (str[i] == 't' || str[i] == 'T')
+			str[i] = '7';
+		else if (str[i] == 'l' || str[i] == 'L')
+			str[i] = '1';
+		else
+			str[i] = str[i];
+	}
+	return (str);
+}
