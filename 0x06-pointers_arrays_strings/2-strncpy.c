d3e190ab2a237b070de8042d65966fa876a8b994
OvyEvbodi <evbodiovo@gmail.com>
2022-09-22 03:06:30 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,19 +12,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	/**
-	* len = 0;
-	*while (src[i++])
-	*{
-	*	len++
-	*}
-	*/
-	for (i = 0; src[i]; i++)
+
+	for (; n; n--)
 	{
-		if (i < n)
-			dest[i] = src[i];
+		dest[i] = src[i];
+		i++;
 	}
-	if (n < i)
-		dest[i] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
