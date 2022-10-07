09f6074115cc1be6a907c4bdf000977a3aae3686
OvyEvbodi <evbodiovo@gmail.com>
2022-10-06 17:58:56 -0700

    realloc using malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,8 +20,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (new_ptr);
 	if (new_size == 0 && new_ptr != NULL)
+	{
 		free(new_ptr);
 		return (NULL);
+	}
 	arr = malloc(new_size);
 	if (arr == NULL)
 		return (NULL);
