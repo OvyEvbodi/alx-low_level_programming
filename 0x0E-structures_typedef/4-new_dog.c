afdca94a014ec42ae3dbf6d9a7dcb47ef6d5eb09
OvyEvbodi <evbodiovo@gmail.com>
2022-10-10 16:49:45 -0700

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,9 +19,52 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (pupp == NULL)
 		return (NULL);
 
-	pupp->name = name;
-	pupp->owner = owner;
+	pupp->name = _strdup(name);
+	pupp->owner = _strdup(owner);
 	pupp->age = age;
     
 	return (pupp);
  }
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ *
+ * @str: the string to copy
+ *
+ * Return: a pointer to the new string on success,
+ * otherwise NULL
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int len = 0, i = 0;
+	char *new_str;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+	{
+		len++;
+	}
+
+	new_str = (char *) malloc(sizeof(char) * (len + 1));
+
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; i < len; i++)
+		{
+			new_str[i] = str[i];
+		}
+	}
+	return (new_str);
+}
