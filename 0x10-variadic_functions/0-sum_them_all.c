29fb21b88b093881bff3af1c483868c0ce4f3624
OvyEvbodi <evbodiovo@gmail.com>
2022-10-13 08:48:31 -0700

    Sum all arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,11 +12,12 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
 	va_list nums;
 	unsigned int i = 0, sum = 0;
 
+	if (n == 0)
+		return (0);
+	
 	for (; i < n; i++)
 	{
 		sum += va_arg(nums, int);
