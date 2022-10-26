6ddf6163b75aa3a246de2460ca35e9ee08ed923f
OvyEvbodi <evbodiovo@gmail.com>
2022-10-25 19:52:41 -0700

    Fix documentation

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,7 +10,7 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *next, *prev;
-	
+
 	next = NULL;
 	prev = NULL;
 
--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,13 +3,14 @@
 /**
  * print_listint_safe - prints a listint_t linked list
  * @head: the head node
- * 
+ *
  * Return: the number of nodes in the list
 */
 
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
+
 	if (head)
 	{
 		count++;
@@ -18,6 +19,6 @@ size_t print_listint_safe(const listint_t *head)
 			count += print_listint_safe(head->next);
 		else
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-	} 
+	}
 	return (count);
 }
