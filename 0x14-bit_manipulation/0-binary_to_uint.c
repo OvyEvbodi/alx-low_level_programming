bc49b997282f246bc555b86600946661a21041f1
OvyEvbodi <evbodiovo@gmail.com>
2022-10-27 14:31:33 -0700

    Optimize documentation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 char *strdup_binary(const char *str, unsigned int *len);
 
 /**
@@ -22,24 +20,24 @@ unsigned int binary_to_uint(const char *b)
 	}
 	new_str =  strdup_binary(b, &len);
 	if (*new_str == 'F')
-		{
-			return (0);
-		}
+	{
+		return (0);
+	}
 	index = len - 1;
 	while (len--)
 	{
 		sum += (new_str[index--] - 48) * weight;
 		weight *= 2;
 	}
-        return (sum);
-	
+	return (sum);
 }
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
+ * strdup_binary - returns a pointer to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter
  *
  * @str: the string to copy
+ * @len: the length variable, set to 0
  *
  * Return: a pointer to the new string on success,
  * otherwise NULL
