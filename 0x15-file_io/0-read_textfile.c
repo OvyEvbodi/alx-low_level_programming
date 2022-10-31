782a667cc7ee4d3ef1125eeb74a5b629b0400644
Ovy-Evbodi <evbodiovo@gmail.com>
2022-10-31 17:30:50 +0100

    Read a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,14 +13,14 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd_o, fd_r, fd;
+	ssize_t fd_o, fd;
 
 	if (filename == NULL)
 		return (0);
 	fd_o = open(filename, O_RDONLY);
-	fd_r = read(fd_o, (void *)filename, letters);
-	fd = write(STDOUT_FILENO, (void *)filename, fd_r);
-	if (fd == -1 || fd_o == -1 || fd_r == -1)
+	fd = read(fd_o, (void *)filename, letters);
+	fd = write(STDOUT_FILENO, (void *)filename, fd);
+	if (fd == -1 || fd_o == -1)
 		return (0);
 	close(fd_o);
 	return (fd);
