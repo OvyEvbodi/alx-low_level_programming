c37aa305397c963abd84e35a7c0f115132ce58ec
Ovy-Evbodi <evbodiovo@gmail.com>
2022-10-31 20:04:37 +0100

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,17 +15,26 @@ int create_file(const char *filename, char *text_content)
 	int fd, fd_w;
 	ssize_t len = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC, 0000600);
-	if (text_content != NULL)
-		while (text_content[len])
-		{
-			len++;
-		}
-	fd_w = write(fd, text_content, len);
-	if (fd == -1 || fd_w == -1)
+	if (fd == -1 )
 		return (-1);
+	if (!text_content)
+		{
+			close(fd);
+			return (1);
+		}
+	while (text_content[len])
+	{
+		len++;
+	}
+	fd_w = write(fd, text_content, len);
+	if (fd_w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 	close(fd);
 	return (1);
 }
