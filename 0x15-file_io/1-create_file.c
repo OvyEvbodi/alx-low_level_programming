dd35538a221ced3706e8218583445e4dd9fb4d8c
Ovy-Evbodi <evbodiovo@gmail.com>
2022-10-31 20:10:40 +0100

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,10 +21,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
-		{
-			close(fd);
-			return (1);
-		}
+	{
+		close(fd);
+		return (1);
+	}
 	while (text_content[len])
 	{
 		len++;
