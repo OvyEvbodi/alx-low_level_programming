7a48a2c3aa33694ce805f29507af13ca855b09be
Ovy-Evbodi <evbodiovo@gmail.com>
2022-10-31 15:44:35 +0100

    Read a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file,
+ * and prints it to the POSIX standard output
+ *
+ * @filename: the file to read
+ * @letters: the number of bytes to be read from the file
+ *
+ * Return: if successful, the number of bytes read,
+ * otherwise, 0.
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd;
+	if (filename == NULL)
+		return (0);
+	fd = read(STDIN_FILENO, filename, letters);
+	if (fd == -1)
+		return (0);
+	return (fd);
+}
