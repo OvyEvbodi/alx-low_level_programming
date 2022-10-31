8f90fa8f91834fee08bac1f8590ee3defa85c1ee
Ovy-Evbodi <evbodiovo@gmail.com>
2022-10-31 15:47:09 +0100

    Read a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd;
 	if (filename == NULL)
 		return (0);
-	fd = read(STDIN_FILENO, filename, letters);
+	fd = read(STDIN_FILENO, (void *)filename, letters);
 	if (fd == -1)
 		return (0);
 	return (fd);
