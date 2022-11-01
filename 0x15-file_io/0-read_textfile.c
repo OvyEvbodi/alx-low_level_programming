75774f88435f179df4f84d6b10bd82b199e36df4
Ovy-Evbodi <evbodiovo@gmail.com>
2022-11-01 01:16:24 +0100

    Add condition for write fail

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,8 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd_o, fd_r, fd_w;
+	ssize_t fd_r, fd_w;
+	int fd_o;
 
 	if (filename == NULL)
 		return (0);
@@ -26,6 +27,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd_o);
 		return (0);
 	}
+	close(fd_o);
 	fd_w = write(STDOUT_FILENO, (void *)filename, fd_r);
 	if (fd_w == -1 || fd_r != fd_w)
 	{
