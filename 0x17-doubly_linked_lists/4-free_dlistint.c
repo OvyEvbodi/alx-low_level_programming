1818e9bc10575dc3786cd76c979b6bd3b5cdf7c6
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-08 12:07:30 +0100

    Fix memory leaks

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -12,10 +12,11 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp = head;
 
-	while (temp)
+	while (temp->next)
 	{
 		temp = temp->next;
 		free (temp->prev);
 	}
+	free(temp->prev);
 	free(temp);
 }
