c8dfea5e42c0525aa2769ef5a8c15aead65f3020
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-08 15:47:53 +0100

    Delete node at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -32,7 +32,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (temp)
 			temp = temp->next;
 		else
+		{
+			free(temp);
 			break;
+		}
 		index--;
 	}
 	temp_prev = temp->prev;
