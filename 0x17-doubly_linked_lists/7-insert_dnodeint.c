aa32013028d79acaae2d47b9f524f0c142e65e1a
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-08 15:41:23 +0100

    Update task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		idx--;
 	}
 	new->next = temp->next;
-	temp->next = new;
 	new->prev = temp;
+	temp->next->prev = new;
+	temp->next = new;
 	return (new);
 }
