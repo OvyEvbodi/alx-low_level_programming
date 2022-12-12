6a014fdcabe879eb7df63940bd661b0c0ec8e6af
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-12 16:36:09 +0100

    Add arithmetic functions

---
+++ 0x18-dynamic_libraries/arit.h
@@ -0,0 +1,12 @@
+#ifndef ARIT_H
+#define ARIT_H
+
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+
+#endif /* ARIT_H */
---
+++ 0x18-dynamic_libraries/arit_ops.c
@@ -0,0 +1,35 @@
+#include "arit.h"
+
+/**
+ * small functions to perform basic arithmetic operations
+ * (+, -, *, /, %) on two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result of whichever operation was called
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
