cd2980e378060b690e66689a901db31517757149
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-07 00:45:48 +0100

    edit header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_TABLES
+#define HASH_TABLES
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,9 +19,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -31,9 +34,10 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
+
 
 /* function prototypes*/
 hash_table_t *hash_table_create(unsigned long int size);
@@ -41,3 +45,5 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int add_node_beg(hash_node_t **new, const char *key, const char *value);
+
+#endif /*HASH_TABLES*/
