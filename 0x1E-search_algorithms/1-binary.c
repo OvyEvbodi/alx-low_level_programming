0bf42a08922dbd927a88f2cb2a847f4d3962d53f
Ovy Evbodi <evbodiovo@gmail.com>
2023-04-16 21:26:02 +0000

    chore: fix indentation

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,9 +16,9 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t start = 0, end = size - 1, pivot = end, i = 0, j = 0;
 
-	if (array == NULL)
-        return (-1);
-    while (end - start >= 1)
+        if (array == NULL)
+                return (-1);
+        while (end - start >= 1)
 	{
 	printf("Searching in array: ");
 		for (i = start, j = end; i <= j; i++)
